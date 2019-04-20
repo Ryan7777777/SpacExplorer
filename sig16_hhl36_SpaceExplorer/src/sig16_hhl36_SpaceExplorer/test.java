37155f8936230912e60ce9afdad51eb40276eb2b
Ryan7777777 <ryanleung7777@gmail.com>
2019-04-21 11:32:11 +1200

    123

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/test.java
+++
@@ -1,8 +0,0 @@
-package sig16_hhl36_SpaceExplorer;
-
-public class test {
-	public static void main(String arg[]) {
-		System.out.println("Don't Panic!");
-		System.out.println("Tekkkst");
-	}
-}
--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/ttr.java
+++
@@ -1,8 +0,0 @@
-package sig16_hhl36_SpaceExplorer;
-
-public class ttr {
-	public static void main(String arg[]) {
-		System.out.println("Don't Panic!");
-		System.out.println("Te216161616sjbkjbjt");
-	}
-}
