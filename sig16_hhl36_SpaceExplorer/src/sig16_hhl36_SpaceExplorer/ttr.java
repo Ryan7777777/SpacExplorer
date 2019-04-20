fd9c533d80df4d5a920d5bdcbe56abb0c463f3a8
Ryan7777777 <ryanleung7777@gmail.com>
2019-04-21 11:31:32 +1200

    12

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/ttr.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/ttr.java
@@ -3,6 +3,6 @@ package sig16_hhl36_SpaceExplorer;
 public class ttr {
 	public static void main(String arg[]) {
 		System.out.println("Don't Panic!");
-		System.out.println("Te21616sjbkjbjt");
+		System.out.println("Te216161616sjbkjbjt");
 	}
 }
