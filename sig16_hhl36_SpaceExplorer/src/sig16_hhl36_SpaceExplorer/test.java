b7af5b3716fe5be777aec369e7493985b362adf7
Ryan7777777 <ryanleung7777@gmail.com>
2019-04-22 15:23:29 +1200

    OK

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/test.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/test.java
@@ -2,6 +2,6 @@ package sig16_hhl36_SpaceExplorer;
 
 public class test {
 	public static void main(String arg[]) {
-		System.out.println("Don't Panic!");
+		System.out.println("DoDADFA't Panic!");
 	}
 }
