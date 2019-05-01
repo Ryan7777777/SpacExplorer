e2dd98a6f2efa5c91ea93260963c1718e8807bc3
Ryan7777777 <ryanleung7777@gmail.com>
2019-05-01 20:13:05 +1200

    class hungyboy basic

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/HungyBoy.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/HungyBoy.java
@@ -1,5 +1,12 @@
 package sig16_hhl36_SpaceExplorer;
 
-public class HungyBoy {
-
+public class HungyBoy extends CrewMenber {
+	public void HungyBoy() {
+		super.CrewMenber("name", "Hungy boy", 10, 40, 4, "no");
+	}
+	public static void main(String arg[]) {
+		HungyBoy boy = new HungyBoy();
+		System.out.println(boy instanceof CrewMenber);
+		
+	}
 }
