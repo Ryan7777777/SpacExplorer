577951bd31411a63b2badb5ef80e26eb35a9092a
Ryan7777777 <ryanleung7777@gmail.com>
2019-05-01 20:31:33 +1200

    basic lazyslepper

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Lazyslepper.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Lazyslepper.java
@@ -1,5 +1,12 @@
 package sig16_hhl36_SpaceExplorer;
 
-public class Lazyslepper {
-
+public class Lazyslepper extends CrewMenber{
+	public void Lazyslepper() {
+		super.CrewMenber("name", "Hungy boy", 10, 40, 4, "no");
+	}
+	public static void main(String arg[]) {
+		Lazyslepper slepper = new Lazyslepper();
+		System.out.println(slepper instanceof CrewMenber);
+		
+	}
 }
