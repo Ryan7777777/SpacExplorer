16a44df752d825b27f1c7dfeeb9d6f01754cc7fe
Ryan7777777 <ryanleung7777@gmail.com>
2019-05-03 10:27:44 +1200

    test array list

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/HungyBoy.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/HungyBoy.java
@@ -1,12 +1,7 @@
 package sig16_hhl36_SpaceExplorer;
 
 public class HungyBoy extends CrewMenber {
-	public void HungyBoy() {
+	public HungyBoy() {
 		super.CrewMenber("name", "Hungy boy", 10, 40, 4, "no");
-	}
-	public static void main(String arg[]) {
-		HungyBoy boy = new HungyBoy();
-		System.out.println(boy instanceof CrewMenber);
-		
 	}
 }
