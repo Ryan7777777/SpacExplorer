208bbb039fa29469cb53f0b759bd76d06c52edcc
Ryan7777777 <ryanleung7777@gmail.com>
2019-05-02 20:58:02 +1200

    GameManager basic

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/GameManager.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/GameManager.java
@@ -1,5 +1,27 @@
 package sig16_hhl36_SpaceExplorer;
 
+import java.util.ArrayList;
+
 public class GameManager {
+	private String shipname;
+	private String crewname;
+	private Double  money = (double) 100;
+	//private ArrayList<CrewMenber> crew_menber = new ArrayList<CrewMenber>();
+	//private ArrayList<Food_Item> crew_food = new ArrayList<Food_Item>();
+	//private ArrayList<MedicalItem> crew_medical = new ArrayList<MedicalItem>();
+	
+	public void setupshipname(String name) {
+		shipname = name;
+	}
+	public void setupcrewname(String name) {
+		crewname = name;
+	}
+	public static void main(String arg[]) {
+		GameManager game = new GameManager();
+		game.setupshipname("testship");
+		game.setupcrewname("testcrew");
+		System.out.printf(game.shipname);
+	}
+	
 
 }
