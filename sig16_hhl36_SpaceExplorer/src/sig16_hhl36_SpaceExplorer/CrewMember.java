1b3924e6994c48f6ae1aab696aa7bdd25f4a2729
saman <saman@192.168.1.15>
2019-05-07 00:27:06 +1200

    changed variable names to java naming convention

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/CrewMember.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/CrewMember.java
@@ -8,57 +8,63 @@ public class CrewMember {
 	private int action;
 	private int healthdroprate;
 	private int hungerrate;
-	private int tirednesrate;
+	private int tirednessrate;
 	private int vitality;
 	private int hungrylevel;
 	private String ability;
-	private boolean sick = false;
+	private boolean sick;
 	
-	public CrewMember(String crew_name, String crew_type, int crew_healthdroprate, int crew_hungerrate, 
-			int crew_tirednesrate, String crew_ability) {
-		name = crew_name;
+	public CrewMember(String memberName, String crewType, int crewHealthdroprate, int crewHungerrate, int crewTirednessrate, String crewAbility) {
+		name = memberName;
 		health = 100;
-		type = crew_type;
+		type = crewType;
 		action = 2;
 		vitality = 100;
 		hungrylevel = 0;
-		healthdroprate = crew_healthdroprate;
-		hungerrate = crew_hungerrate;
-		tirednesrate = crew_tirednesrate;
-		ability = crew_ability;
+		healthdroprate = crewHealthdroprate;
+		hungerrate = crewHungerrate;
+		tirednessrate = crewTirednessrate;
+		ability = crewAbility;
+		sick = false;
 	}
+	
 	public void sick() {
 		sick = true;
 		healthdroprate = 20;
 	}
+	
 	public void recovery() {
 		sick = false;
 		healthdroprate = 10;
 	}
+	
 	public void sleep() {
-		if((vitality + 20) < 100) {
+		if ((vitality + 20) < 100) {
 			vitality += 20;
-		} else{
+		} else {
 			vitality = 100;
 		}
 	}
+	
 	public void addhealth(int addedamount) {
-		if(health + addedamount < 100) {
+		if (health + addedamount < 100) {
 			health += addedamount;
-		}else {
+		} else {
 			health = 100;
 		}
 	}
+	
 	public void eat(int nutritionamount) {
-		if(hungrylevel - nutritionamount > 0) {
+		if (hungrylevel - nutritionamount > 0) {
 			hungrylevel -= nutritionamount;
 		} else {
 			hungrylevel = 0;
 		}
 	}
+	
 	public String viewtype() {
 		System.out.println(type);
 		return type;
 	}
-	}
+}
 
