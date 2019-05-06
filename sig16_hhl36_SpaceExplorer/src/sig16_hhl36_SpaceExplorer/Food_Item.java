665fb42bc6bbac3143dd23bb2eff9a133e9005ed
saman <saman@192.168.1.15>
2019-05-07 00:21:57 +1200

    updated food_item class with get methods

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Food_Item.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Food_Item.java
@@ -2,13 +2,26 @@ package sig16_hhl36_SpaceExplorer;
 
 public class Food_Item {
 	
-	private double food_price;
-	private int food_nutrition;
-	private String food_name;
+	private double foodPrice;
+	private int foodNutrition;
+	private String foodName;
 	
 	public Food_Item(double price, int nutrition, String name) {
-		food_price = price;
-		food_nutrition = nutrition;
-		food_name = name;
+		foodPrice = price;
+		foodNutrition = nutrition;
+		foodName = name;
 	}
+	
+	public double getPrice() {
+		return foodPrice;
+	}
+	
+	public int getFoodNutrition() {
+		return foodNutrition;
+	}
+	
+	public String getMedName() {
+		return foodName;
+	}
+	
 }
