282e12a8eb828831871e2c99aa8083e584d56ade
Ryan7777777 <ryanleung7777@gmail.com>
2019-05-18 15:22:59 +1200

    remove test ease

--- sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Superman.java
+++ sig16_hhl36_SpaceExplorer/src/sig16_hhl36_SpaceExplorer/Superman.java
@@ -4,8 +4,4 @@ public class Superman extends CrewMember{
 	public Superman() {
 		super("name", "Superman", 4, 4, 4, "Everything is easy for him");
 	}
-	public static void main(String arg[]) {
-		Superman superman = new Superman();
-		System.out.println(superman instanceof CrewMember);
-	}
 }
